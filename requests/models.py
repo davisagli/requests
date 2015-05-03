77cd817a9ad4b000f1439f4aa6be72b28f9837f9
Ian Cordasco <graffatcolmingov@gmail.com>
2015-05-03 14:18:37 -0500

    Remove the __bool__ and __nonzero__ response methods
    Many people expect to be able to say:
    
        response = make_request(url)
    
        if response:
            body = response.content
    
    Where the first part should test for whether or not response is None.
    Instead, the __bool__ and __nonzero__ methods return response.ok, so if
    the response is actually a 4xx or 5xx response, then the user would
    expect to get the body of the response.
    
    By removing these methods, we restore the functionality that most users
    expect.
    
    Closes #2002

--- requests/models.py
+++ requests/models.py
@@ -745,26 +745,6 @@ class Response:
     def __repr__(self):
         return f"<Response [{self.status_code}]>"
 
-    def __bool__(self):
-        """Returns True if :attr:`status_code` is less than 400.
-
-        This attribute checks if the status code of the response is between
-        400 and 600 to see if there was a client error or a server error. If
-        the status code, is between 200 and 400, this will return True. This
-        is **not** a check to see if the response code is ``200 OK``.
-        """
-        return self.ok
-
-    def __nonzero__(self):
-        """Returns True if :attr:`status_code` is less than 400.
-
-        This attribute checks if the status code of the response is between
-        400 and 600 to see if there was a client error or a server error. If
-        the status code, is between 200 and 400, this will return True. This
-        is **not** a check to see if the response code is ``200 OK``.
-        """
-        return self.ok
-
     def __iter__(self):
         """Allows you to use a response as an iterator."""
         return self.iter_content(128)
